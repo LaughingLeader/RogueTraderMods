7204a9c6dcfe43eef11e6b91e96a0a75f4845388
LaughingLeader <laughingleader@gmail.com>
2023-12-12 16:43:41 -0600

    Specify unit so message can be clicked
    LogHelper.GetEntityName also colors the name like regular messages

--- src/Mods/MoreInfo/Mod/Patches/CombatLogPatch.cs
+++ src/Mods/MoreInfo/Mod/Patches/CombatLogPatch.cs
@@ -10,8 +10,10 @@ using Kingmaker.EntitySystem.Stats;
 using Kingmaker.EntitySystem.Stats.Base;
 using Kingmaker.Mechanics.Entities;
 using Kingmaker.RuleSystem.Rules;
+using Kingmaker.TextTools;
 using Kingmaker.UI.Models.Log.CombatLog_ThreadSystem;
 using Kingmaker.UI.Models.Log.CombatLog_ThreadSystem.LogThreads.Combat;
+using Kingmaker.UI.Models.Log.Enums;
 using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.Abilities;
 
@@ -23,6 +25,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+
+using UnityEngine;
 
 namespace MoreInfo.Mod.Patches
 {
@@ -71,27 +75,51 @@ namespace MoreInfo.Mod.Patches
 			harmony.Unpatch(m_RollInitiative3, thisType.GetMethod(nameof(DebugUpdateBuffsInitiative)));*/
 		}
 
-		private static readonly string InitiativeMessage = "{0}: {1} ((AGI: {2}, PER: {3})";
+		private static readonly string InitiativeMessage = "{0}: Rolled {1} initiative";
+		private static readonly string InitiativeMessageWithBonuses = "{0}: Rolled {1} initiative ({2} AGI, {3} PER)";
 
-		//private static void OnInitiativeRollingDone(ref IEnumerable<MechanicEntity> newCombatants, ref bool relax)
-		private static void OnInitiativeRollingDone(ref List<MechanicEntity> ___m_JoinedThisTickEntities)
+		private static void AddMessagesAsOne(List<MechanicEntity> ___m_JoinedThisTickEntities)
 		{
 			if (___m_JoinedThisTickEntities.Count > 0)
 			{
 				var tooltip = new TooltipTemplateGlossary("Initiative");
 				var thread = InGameLog.GetThread<UnitInitiativeLogThread>(LogChannelType.AnyCombat);
+				var messages = new List<string>() { "Initiative Rolls:" };
+				InGameLog.AddMessage(thread, "Initiative Rolls:", PrefixIcon.RightArrow, tooltip);
 				foreach (var entity in ___m_JoinedThisTickEntities.OrderByDescending(x => x.Initiative.Roll))
 				{
 					if (entity != null)
 					{
 						var agilityBonus = entity.GetStatOptional<ModifiableValueAttributeStat>(StatType.WarhammerAgility)?.WarhammerBonus ?? 0;
 						var perceptionBonus = entity.GetStatOptional<ModifiableValueAttributeStat>(StatType.WarhammerPerception)?.WarhammerBonus ?? 0;
-						var msg = String.Format(InitiativeMessage, entity.Name, Math.Floor(entity.Initiative.Roll), agilityBonus, perceptionBonus);
-						InGameLog.AddMessage(thread, msg, Kingmaker.UI.Models.Log.Enums.PrefixIcon.None, tooltip);
+						messages.Add(String.Format(InitiativeMessageWithBonuses, entity.Name, Math.Floor(entity.Initiative.Roll), agilityBonus, perceptionBonus));
+					}
+				}
+				InGameLog.AddMessage(thread, String.Join(Environment.NewLine, messages), PrefixIcon.None, tooltip);
+			}
+		}
+
+
+		//private static void OnInitiativeRollingDone(ref IEnumerable<MechanicEntity> newCombatants, ref bool relax)
+		private static void OnInitiativeRollingDone(ref List<MechanicEntity> ___m_JoinedThisTickEntities)
+		{
+			if (___m_JoinedThisTickEntities.Count > 0)
+			{
+				var thread = InGameLog.GetThread<UnitInitiativeLogThread>(LogChannelType.AnyCombat);
+				if (thread == null) return;
+				var tooltip = new TooltipTemplateGlossary("Initiative");
+				//InGameLog.AddMessage(thread, "Initiative Rolls:", PrefixIcon.RightArrow, tooltip);
+				foreach (var entity in ___m_JoinedThisTickEntities.OrderByDescending(x => x.Initiative.Roll))
+				{
+					if (entity != null)
+					{
+						var name = LogHelper.GetEntityName(entity);
+						var str = String.Format(InitiativeMessage, name, Math.Floor(entity.Initiative.Roll));
+						var message = new CombatLogMessage(str, Color.black, PrefixIcon.None, tooltip, true, unit: entity);
+						thread.AddMessage(message);
 					}
 				}
 			}
-
 		}
 
 		private static string PrintInitiative(MechanicEntity entity)
