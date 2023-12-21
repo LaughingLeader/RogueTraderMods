e2e88e294695104fb752d9f671acf56899231914
LaughingLeader <laughingleader@gmail.com>
2023-12-21 11:07:23 -0600

    Reorganize settings

--- src/Mods/LeaderTweaks/Mod/PluginSettings.cs
+++ src/Mods/LeaderTweaks/Mod/PluginSettings.cs
@@ -12,20 +12,16 @@ namespace LeaderTweaks.Mod
 {
 	public class PluginSettings : UnityModManager.ModSettings, IDrawable
 	{
-		[Header("Combat")]
-		[Draw("Relentless Blaze: Automatically Apply Burning")] public bool AutoRelentlessBlaze = false;
-		[Draw("Relentless Blaze: No Burning Damage")] public bool RelentlessBlazeBurningDealsZeroDamage = false;
-
-		[Header("Party")]
-		[Draw("Max Party Size", Min = 1, Max = 24)]
-		public int MaxPartySize = 12;
-
 		[Header("Cheats")]
 		[Draw("Non-Combat Roll Checks", Collapsible = true)]
 		public RollCheckSettings RollChecks = new();
 
-		[Draw("Fixes", Collapsible = true)]
-		public FixSettings Fixes = new();
+		[Header("Tweaks")]
+		[Draw("Max Party Size", Tooltip="This will expand the max party size in the recruitment screen as well", Min = 1, Max = 24)]
+		public int MaxPartySize = 10;
+
+		[Draw("Talents", Collapsible = true)]
+		public TalentSettings Talents = new();
 
 		public override void Save(UnityModManager.ModEntry modEntry)
 		{
@@ -34,22 +30,35 @@ namespace LeaderTweaks.Mod
 
 		public void OnChange()
 		{
+			
+		}
 
+		public PluginSettings() : base()
+		{
+			var t = GetType();
+			//Expand by default
+			UnityModManager.UI.collapsibleStates.Add(t.GetField(nameof(Talents)).MetadataToken);
+			UnityModManager.UI.collapsibleStates.Add(t.GetField(nameof(RollChecks)).MetadataToken);
 		}
 	}
 
-	[DrawFields(DrawFieldMask.Public)]
-	public class FixSettings
+	public class TalentSettings
 	{
-		[Draw("Prevent Pain Channeling Self Damage", Tooltip = "Fix damaging yourself with Pain Channeling when killing the last nearby enemy")]
+		[Draw("Relentless Blaze: Automatically Apply Warp Burn/Orchestrate Flames", Tooltip = "When combat starts, automatically apply the following buffs if the related abilities are available:\nIgnite: Warp Burn\nOrchestrate Flames: Orchestrate Flames buff\nThis will trigger the +1 Psy Rating bonus")]
+		public bool AutoRelentlessBlaze = false;
+
+		[Draw("Relentless Blaze: No Burning Damage", Tooltip = "Disable damage from Burning/Warp Burn if the character has the Relentless Blaze talent and the Orchestrate Flames buff")]
+		public bool RelentlessBlazeBurningDealsZeroDamage = false;
+
+		[Draw("Pain Channeling: Prevent Self Damage", Tooltip = "Fix damaging yourself with Pain Channeling when killing the last nearby enemy")]
 		public bool DisablePainChannelingSelfDamage = true;
 	}
 
 	public class RollCheckSettings
 	{
-		[Draw("Always Pass Skill Checks", Tooltip = "Allows always succeeding in skill checks out of combat, where otherwise you might have a 0% chance")]
+		[Draw("Always Pass Skill Checks", Tooltip = "Always succeed in skill checks out of combat, even if you have a 0% chance")]
 		public bool NonCombatSkillChecks = false;
-		[Draw("Always Pass Attribute Checks", Tooltip = "Allows always succeeding in attribute checks out of combat, where otherwise you might have a 0% chance")]
+		[Draw("Always Pass Attribute Checks", Tooltip = "Always succeed in attribute checks out of combat, even if you have a 0% chance")]
 		public bool NonCombatAttributeChecks = false;
 
 		public bool IsEnabled => NonCombatAttributeChecks || NonCombatSkillChecks;
