fcc1e106dbbef2a8620c2e991c14588bb8a11f1c
LaughingLeader <laughingleader@gmail.com>
2023-12-26 16:29:58 -0600

    Update Repository.json via zip.py

--- zip.py
+++ zip.py
@@ -3,9 +3,15 @@ from pathlib import Path
 import zipfile
 import argparse
 import shutil
+import dataclasses
+import json
+from dataclass_wizard import JSONWizard
 
 script_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(script_dir)
+
+repo_user = "LaughingLeader"
+repo_name = "RogueTraderMods"
 
 publish_dir = script_dir.joinpath("publish")
 latest_dir = publish_dir.joinpath("latest")
@@ -19,11 +25,12 @@ args = parser.parse_args()
 release_dir:Path = Path(args.directory.replace('"', ""))
 release_version:str = args.version
 
-print(release_dir)
-
 project_name = release_dir.name
-release_zip_file = publish_dir.joinpath(f"{project_name}_v{release_version}.zip")
+zip_name = f"{project_name}_v{release_version}.zip"
+release_zip_file = publish_dir.joinpath(zip_name)
 release_dir_str = str(release_dir)
+
+download_url = f"https://github.com/{repo_user}/{repo_name}/releases/download/v{release_version}-{project_name}/{zip_name}"
 
 release_zip_file.parent.mkdir(parents=True, exist_ok=True)
 
@@ -37,3 +44,30 @@ latest_dir.mkdir(parents=True, exist_ok=True)
 shutil.copyfile(str(release_zip_file), str(latest_dir.joinpath(f"{project_name}.zip")))
 
 print(f"Zipped project: {release_zip_file}")
+
+@dataclasses.dataclass
+class RepositoryReleaseEntry:
+    Id:str
+    Version:str
+    DownloadUrl:str
+
+@dataclasses.dataclass
+class RepositoryData(JSONWizard):
+    Releases:list[RepositoryReleaseEntry] = dataclasses.field(default_factory=list)
+
+    def get_entry(self, name:str, version:str)->RepositoryReleaseEntry|None:
+        for entry in self.Releases:
+            if entry.Id == name and entry.Version == version:
+                return entry
+
+releases_json = script_dir.joinpath("Repository.json")
+if releases_json.exists():
+	data = json.loads(releases_json.read_text(encoding="utf-8"))
+	releases_data = RepositoryData.from_dict(data)
+	needs_save = False
+	entry = releases_data.get_entry(project_name, release_version)
+	if not entry:
+		releases_data.Releases.append(RepositoryReleaseEntry(project_name, release_version, download_url))
+		needs_save = True
+	releases_data.Releases.sort(key=lambda entry: (entry.Id, entry.Version))
+	releases_json.write_text(json.dumps(dataclasses.asdict(releases_data), indent="\t"), encoding="utf-8")
